ef7180443c31cc9026797b59270e9a1404b2bdb1
ALİ <AL@LENOVO-PC>
2022-06-07 22:37:20 +0300

    Add project files.

---
+++ İlkDers/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace İlkDers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Kenar uzunluğu giriniz: ");
+            int kenar = int.Parse(Console.ReadLine());
+
+            int alan = kenar * kenar;
+
+            Console.WriteLine("Karenin alanı: "+alan);
+
+            int cevre = 4 * kenar;
+
+            Console.WriteLine("Karenin çevresi: "+cevre);
+
+
+        }
+    }
+}
