68092e4d397540d125248f929888d07d1c2fcee4
aliturkmen4 <alym11731996@gmail.com>
2022-06-10 17:40:08 +0300

    convert method application was made

---
+++ ConvertMethodlari/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConvertMethodlari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi = 12;
+            string sayiString = "12";
+
+            bool varmi = false;
+            string varmiString = "false";
+
+            Console.WriteLine(sayiString+2);
+
+            int sonuc1 = Convert.ToInt32(sayiString);
+
+            Console.WriteLine(sonuc1+2 );
+
+            bool sonuc2 = Convert.ToBoolean(varmiString);
+            Console.WriteLine(sonuc2);
+        }
+    }
+}
