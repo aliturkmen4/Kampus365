a2ad812985cf4c13ecffb5b9335f349bfc903fe0
aliturkmen4 <alym11731996@gmail.com>
2022-06-12 23:55:32 +0300

    Recursive Method

---
+++ RecursiveMethods/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace RecursiveMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktöriyel Hesaplama");
+            Console.WriteLine("********************");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Lütfen bir sayı giriniz");
+
+            int sayi=int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine(sayi+"!= "+Faktoriyel(sayi));
+
+        }
+        //recursive => özyinelemeli
+        static int Faktoriyel(int deger)
+        {
+            if (deger == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return deger * Faktoriyel(deger - 1); //methodun içinde aynı methodu kullandım (özyineleme)!!
+            }          
+        }
+    }
+}
