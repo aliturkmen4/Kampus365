5da8bd1040c2c38d482fe3518e120c5b1fccbc94
aliturkmen4 <alym11731996@gmail.com>
2022-06-13 00:02:22 +0300

    DoWhileLoop

---
+++ DoWhileLoop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DoWhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayac = 0;
+
+            //döngü her halkuarda 1 kere çalışır, her çalışmadan sonra while ın içini sağlayıp sağlamadığını kontrol eder!
+
+            do
+            {
+                Console.WriteLine("Sayaç: "+sayac);
+                sayac++;
+            } while (sayac<10);
+        }
+    }
+}
