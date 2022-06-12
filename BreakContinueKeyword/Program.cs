e2d172cfa77d02c4ac7ae339f6e93789dff4808d
aliturkmen4 <alym11731996@gmail.com>
2022-06-13 00:14:41 +0300

    Break/Continue Keywords

---
+++ BreakContinueKeyword/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace BreakContinueKeyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 2)
+                {
+                    continue; //kodu döngü kısmına atar o yüzden ekranda 2 yi görmeyiz!
+                }
+
+                Console.WriteLine("Sayaç: "+i);
+
+                if (i == 5)
+                {
+                    break; //direkt olarak döngünün sonlanmasını sağlar!
+                }
+            }
+        }
+    }
+}
