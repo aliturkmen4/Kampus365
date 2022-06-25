ce90be8c22e4b0f8b667336a25bce11d3a10091f
aliturkmen4 <alym11731996@gmail.com>
2022-06-25 22:22:06 +0300

    Class concept was studied

---
+++ ClassConcept/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassConcept
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
